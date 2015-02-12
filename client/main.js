72239c8e27c83f47d94ede63288708656bad6e4c
Tommyluo <tluo12321@yahoo.com>
2015-02-12 15:46:13 -0800

    work in progress

--- client/main.js
+++ client/main.js
@@ -1,1 +1,35 @@
-console.log('hello, world');
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+  $('#get-data').click(clickGetData);
+  $('#get-type').click(clickGetType);
+}
+
+function clickGetData() {
+  var year = $('select').val();
+  var url = 'https://cdph.data.ca.gov/resource/kbup-p858.json?year=' + year;
+  $.getJSON(url, function(response) {
+
+  var homocides = _.map(response, function(o){return o.hom * 1});
+  homocides = _.compact(homocides);
+  homocides = _.reduce(homocides, function(prev, curr) {return prev + curr});
+  // debugger;
+    console.log(homocides);
+  })
+}
+
+  function clickGetType() {
+    var year = $('#year').val();
+    var type = $('#type').val();
+    var url = 'https://cdph.data.ca.gov/resource/kbup-p858.json?type=' + type;
+    $.getJSON(url, function(response) {
+
+    var types = _.map(response, function(o){return o[types] * 1});
+    types = _.compact(types);
+    types = _.reduce(types, function(prev, curr) {return prev + curr});
+    // debugger;
+      console.log(types);
+    })
+}
